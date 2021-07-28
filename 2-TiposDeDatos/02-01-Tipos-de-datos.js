13294161ccd11bdad9be5e1d9d5c49f08428823e
DanyGuerra <luis_daniel.grra15@hotmail.com>
2021-07-28 13:45:09 -0500

    Ejemplo de tipos de datos

---
+++ 2-TiposDeDatos/02-01-Tipos-de-datos.js
@@ -0,0 +1,17 @@
+
+//Tipo de dato String
+var nombre = "Carlos";
+console.log('Dato String: ', nombre);
+
+// Number
+var numero = 1000;
+console.log('Dato Number: ',numero);
+
+//Objeto
+var objeto = {
+    nombre: "Juan",
+    apellido: "Perez",
+    telefono: "4641668507"
+};
+
+console.log("Dato Object: ", objeto);
